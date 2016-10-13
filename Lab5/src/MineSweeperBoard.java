e610cfd69f483e11296978ba946fac024cb32b42
acasa <acasa@DESKTOP-7GS6D43>
2016-10-12 21:05:04 -0400

    for loops for the mine search on the 8 squares

--- Lab5/src/MineSweeperBoard.java
+++ Lab5/src/MineSweeperBoard.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 import java.util.Random;
 
 public class MineSweeperBoard {
-	
+
 	public MineSweeperObject[][] mineSweeperGameBoard;
 	public int xBoardSize;
 	public int yBoardSize;
@@ -11,8 +11,8 @@ public class MineSweeperBoard {
 	public int xPosition;
 	public int yPosition;
 	public static final int MINEBOUNDS = 6;
-	
-	
+
+
 	//Constructor for MineSweeperBoard
 	public MineSweeperBoard(int xSize, int ySize){
 		this.xBoardSize = xSize;
@@ -30,27 +30,33 @@ public class MineSweeperBoard {
 			System.out.println("Game Over");
 			return true;
 		}else {
-			for(int i = this.xBoardSize -1 ; i < this.xBoardSize + 1; i++){
-				for(int j = this.yBoardSize - 1; j < this.yBoardSize +1; j++){
-					if(i == this.xBoardSize || j == this.yBoardSize){
-						
+			int counterOfMines = 0;
+			for(int i = this.xPosition - 1 ; i < this.xPosition + 1; i++){
+				for(int j = this.yPosition - 1; j < this.yPosition + 1; j++){
+					if(i == this.xPosition || j == this.yPosition){
+						//does nothing just keeps searching
+
+					}else {
+						//counts the number of mines on the radius of the selected sqaure
+						if(this.mineSweeperGameBoard[xPosition][yPosition].getObjectColor().equals(Color.BLACK)){
+							counterOfMines = counterOfMines + 1;
+						}
 					}
-				}
+				}System.out.println(" the number of mines surrunding it are: " + counterOfMines);
 			}
-				
-			}
-		
+
+		}
 		return false;
-		// There is a bomb on the 8 sqaures surrounding the press sqaure
-			
+		// There is a bomb on the 8 squares surrounding the press square
+
 	}
-	
+
 	//Method: Return Color of the mine that is on the specified position
 	public Color giveCurrentMineColor(int xPosition, int yPosition){
 		return this.mineSweeperGameBoard[xPosition][yPosition].getObjectColor(); 
 	}
-	
-	
+
+
 	//Method: Add the mines in a random fashion to the board
 	public void populateBoard(){
 		System.out.println("Commencing to Populate the Board");
@@ -64,10 +70,11 @@ public class MineSweeperBoard {
 			System.out.println("Mine" + i + ": " + randomXPos +", " + randomYPos);
 			addMineSweeperObjectToBoard(new MineSweeperObject(), randomXPos, randomYPos);
 			System.out.println(mineSweeperGameBoard[randomXPos][randomYPos]);
+
 		}
-		
+
 	}
-	
-	
-	
+
+
+
 }
